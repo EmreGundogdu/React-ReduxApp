aa9ac639972622d752ca0614f9443e7376efca33
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-01-18 12:02:46 +0300

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Counter from "./components/Counter"
+import DecreaseCounter from "./components/DecreaseCounter"
+import IncreaseByTwoCounter from "./components/IncreaseByTwoCounter"
+import IncreaseCounter from "./components/IncreaseCounter"
+
+function App() {
+  return (
+    <div >
+      <Counter/>
+      <IncreaseCounter />
+      <DecreaseCounter />
+      <IncreaseByTwoCounter />
+    </div>
+  );
+}
+
+export default App;
